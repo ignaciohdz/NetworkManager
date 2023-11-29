12c9058d44dff47a7b49e44e686b2dbeab0fda69
Ignacio H <ignacio.hernandez@citi.com.mx>
2023-11-28 19:02:10 -0600

    TA # Generate Framework with Podspect

---
+++ NetworkManagerSDK/NetworkManagerSDK/NetworkManagerSDK.h
@@ -0,0 +1,18 @@
+//
+//  NetworkManagerSDK.h
+//  NetworkManagerSDK
+//
+//  Created by Ignacio Hernandez Montes on 28/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkManagerSDK.
+FOUNDATION_EXPORT double NetworkManagerSDKVersionNumber;
+
+//! Project version string for NetworkManagerSDK.
+FOUNDATION_EXPORT const unsigned char NetworkManagerSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkManagerSDK/PublicHeader.h>
+
+
